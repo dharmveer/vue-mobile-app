a5003d66ab450539dfe046c577fed7da2706094d
Dharmveer Kumar <dharm187@in.ibm.com>
2020-08-22 20:03:52 +0530

    second-commit

--- App.vue
+++ App.vue
@@ -1,9 +1,22 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">River Walk Kitchen</text>
+   
+    <Home/>
   </view>
 </template>
 
+<script>
+import Vuex from 'vuex';
+import Vue from 'vue-native-core';
+import store from "./src/store";
+Vue.prototype.$store = store;
+import Home from './src/screens/Home.vue';
+export default {
+  components: {
+      Home
+    },
+}
+</script>
 <style>
 .container {
   background-color: white;
--- src/screens/Home.vue
+++ src/screens/Home.vue
@@ -1,9 +1,20 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">River Walk Kitchen Home Screen</text>
-  </view>
+   <scroll-view :content-container-style="{contentContainer: {
+        paddingVertical: 20
+    }}">
+     <text class="container" v-for="todo in todos" :key="todo.id">
+    {{ todo.title }}
+  </text>
+        <!-- Content goes here -->
+    </scroll-view>
 </template>
-
+<script>
+import Vue from 'vue-native-core'
+//Vue.store.prototype.$store = store
+export default {
+  
+}
+</script>
 <style>
 .container {
   background-color: white;
@@ -15,3 +26,22 @@
   color: blue;
 }
 </style>
+<script>
+  import axios from 'axios'
+  export default {
+    data () {
+      return {
+        title: 'Home Screen!'
+      }
+    },
+    created () {
+      this.$store.dispatch('fetchTodos')
+    },
+    computed: {
+      // 5. Get todos from state
+      todos () {
+        return this.$store.state.todos
+      }
+    },
+  }
+</script>
